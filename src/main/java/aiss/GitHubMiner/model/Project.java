547348f7e310a038a5ebdbe0e2866752f15b312d
Kourgol <enriqueramosmartin@gmail.com>
2025-04-18 13:27:35 +0200

    model/Project

---
+++ src/main/java/aiss/GitHubMiner/model/Project.java
@@ -0,0 +1,45 @@
+package aiss.GitHubMiner.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Project {
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("web_url")
+    private String web_url;
+
+    //Constructor
+    public Project(String id, String name, String web_url) {
+        this.id = id;
+        this.name = name;
+        this.web_url = web_url;
+    }
+    //Getters & Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getWeb_url() {
+        return web_url;
+    }
+
+    public void setWeb_url(String web_url) {
+        this.web_url = web_url;
+    }
+}
