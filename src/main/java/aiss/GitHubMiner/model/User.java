ff8d8be5c8b00ab1ebad9e01a6bc3b263900b6ea
Irene Martín <irenemartin1805@gmail.com>
2025-04-23 11:37:27 +0200

    User

--- src/main/java/aiss/GitHubMiner/model/User.java
+++ src/main/java/aiss/GitHubMiner/model/User.java
@@ -10,17 +10,65 @@ public class User {
     @JsonProperty("id")
     public String id;
 
-    @JsonProperty("login")
+    @JsonProperty("username")
     public String username;
 
     @JsonProperty("name")
     public String name;
 
     @JsonProperty("avatar_url")
-    public String avatarUrl;
+    public String avatar_url;
 
-    @JsonProperty("url")
-    public String webUrl;
+    @JsonProperty("web_url")
+    public String web_url;
+
+    public User(String id, String username, String name, String avatar_url, String web_url) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.avatar_url = avatar_url;
+        this.web_url = web_url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatarUrl() {
+        return avatar_url;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatar_url = avatarUrl;
+    }
+
+    public String getWebUrl() {
+        return web_url;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.web_url = webUrl;
+    }
 
     @Override
     public String toString() {
@@ -28,8 +76,8 @@ public class User {
                 "id='" + id + '\'' +
                 ", username='" + username + '\'' +
                 ", name='" + name + '\'' +
-                ", avatarUrl='" + avatarUrl + '\'' +
-                ", webUrl='" + webUrl + '\'' +
+                ", avatarUrl='" + avatar_url + '\'' +
+                ", webUrl='" + web_url + '\'' +
                 '}';
     }
 }
