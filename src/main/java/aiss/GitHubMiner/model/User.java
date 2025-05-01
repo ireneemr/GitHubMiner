f0b6c253546080ac97542b6a83ff7cf7ff4a2da4
Irene Martín <irenemartin1805@gmail.com>
2025-05-01 08:46:45 +0200

    User

--- src/main/java/aiss/GitHubMiner/model/User.java
+++ src/main/java/aiss/GitHubMiner/model/User.java
@@ -8,19 +8,59 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class User {
 
     @JsonProperty("id")
-    public String id;
+    private String id;
 
     @JsonProperty("login")
-    public String username;
+    private String username;
 
     @JsonProperty("name")
-    public String name;
+    private String name;
 
     @JsonProperty("avatar_url")
-    public String avatarUrl;
+    private String avatarUrl;
 
     @JsonProperty("url")
-    public String webUrl;
+    private String webUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
 
     @Override
     public String toString() {
